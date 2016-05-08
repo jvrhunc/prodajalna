6224ff056def5690124822b8c1c83d224cb58c4a
Jernej Vrhunc <jv4739@student.uni-lj.si>
2016-05-08 08:42:34 +0000

    Zaključek dela na II. nalogi

--- eracun.js
+++ eracun.js
@@ -26,6 +26,7 @@ streznik.use(
   })
 );
 
+
 var razmerje_usd_eur = 0.877039116;
 
 function davcnaStopnja(izvajalec, zanr) {
@@ -47,7 +48,7 @@ function davcnaStopnja(izvajalec, zanr) {
 
 // Prikaz seznama pesmi na strani
 streznik.get('/', function(zahteva, odgovor) {
-  pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
+    pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
           Artist.Name AS izvajalec, Track.UnitPrice * " +
           razmerje_usd_eur + " AS cena, \
           COUNT(InvoiceLine.InvoiceId) AS steviloProdaj, \
@@ -62,11 +63,16 @@ streznik.get('/', function(zahteva, odgovor) {
           LIMIT 100", function(napaka, vrstice) {
     if (napaka)
       odgovor.sendStatus(500);
-    else {
+      
+    if(!napaka){
+      if(!zahteva.session.user){
+        odgovor.redirect('/prijava');
+      } else {
         for (var i=0; i<vrstice.length; i++)
           vrstice[i].stopnja = davcnaStopnja(vrstice[i].izvajalec, vrstice[i].zanr);
         odgovor.render('seznam', {seznamPesmi: vrstice});
       }
+    }
   })
 })
 
@@ -233,13 +239,15 @@ streznik.post('/stranka', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
-    odgovor.redirect('/')
+    zahteva.session.user = polja.seznamStrank;
+    odgovor.redirect('/');
   });
 })
 
 // Odjava stranke
 streznik.post('/odjava', function(zahteva, odgovor) {
-    odgovor.redirect('/prijava') 
+    zahteva.session.user = "";
+    odgovor.redirect('/prijava');
 })
 
 
